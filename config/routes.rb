654742ec7c25ececf97918c99b96512f96b6a610
JuankStack <Juankstack@gmail.com>
2018-09-21 16:41:42 -0500

    agregamos gemas al app devise y figaro

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'spotify/search'
   resources :tracks
   resources :playlists
+  root 'tracks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
